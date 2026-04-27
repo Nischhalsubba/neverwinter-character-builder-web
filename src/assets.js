5b6202de23e51289e199ebb4057366ada412461e
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-04-28 05:43:41 +0545

    Add optional asset mapping with safe generated icon fallbacks

---
+++ src/assets.js
@@ -0,0 +1,11 @@
+window.NW_ASSETS={
+  notes:'Optional asset map. Add approved/local image URLs here. The app falls back to generated icon chips when no image exists.',
+  items:{
+    'POW':{icon:'P'},'ACC':{icon:'A'},'CA':{icon:'CA'},'C STR':{icon:'CS'},'C SEV':{icon:'CV'},'FOR':{icon:'F'},'OGH':{icon:'OH'},
+    'DEF':{icon:'D'},'AWARE':{icon:'AW'},'C AVOID':{icon:'AV'},'Damage':{icon:'DMG'},'Magnitude':{icon:'MAG'},
+    'Hot Wings':{icon:'HW'},'Squash Soup':{icon:'SS'},'Sorbet':{icon:'SB'},'Flask':{icon:'FL'},'Wild Storm':{icon:'WS'},
+    'Mystic Aura':{icon:'MA'},'Runic Aura':{icon:'RA'},'Pack #1':{icon:'P1'},'Pack #2':{icon:'P2'},'Avian Aura(s)':{icon:'AA'},
+    'Dragonborn':{icon:'DB'},'Human':{icon:'HU'},'Half Orc':{icon:'HO'},'Aasimar':{icon:'AS'},'Gith':{icon:'GI'},'Wood Elf':{icon:'WE'},'Half Elf':{icon:'HE'},'Dwarf':{icon:'DW'},
+    'Barbarian':{icon:'BB'},'Bard':{icon:'BD'},'Cleric':{icon:'CL'},'Fighter':{icon:'FT'},'Paladin':{icon:'PL'},'Ranger':{icon:'RG'},'Rogue':{icon:'RO'},'Warlock':{icon:'WL'},'Wizard':{icon:'WZ'}
+  }
+};
